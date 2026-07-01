b5ba7354997a8f730146590b14053e6ea24c70b9
Devkalion <hyndavova777@gmail.com>
2026-07-02 00:32:28 +0300

    Create telegram sender service

--- services/telegram_sender.py
+++ services/telegram_sender.py
@@ -1,3 +1,15 @@
+from aiogram import Bot
 import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+
+bot = Bot(token=BOT_TOKEN)
+
+async def send_alert(text):
+    msg = await bot.send_message(
+        CHAT_ID,
+        text,
+        parse_mode="HTML"
+    )
+    print(f"MESSAGE SENT: {msg.message_id}")
